54977b8f10c8e273a92cb04120609ab0a33b9373
James Goff <james_goff88@outlook.com>
2020-03-14 11:32:29 -0500

    run build

--- src/config.js
+++ src/config.js
@@ -6,13 +6,13 @@ export default {
   oidc: {
     clientId: CLIENT_ID,
     issuer: ISSUER,
-    redirectUri: 'http://localhost:8080/implicit/callback',
+    redirectUri: 'http://localhost:8080/implicit/callback' || 'https://okta-profile-app.herokuapp.com/implicit/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: OKTA_TESTING_DISABLEHTTPSCHECK,
   },
   resourceServer: {
-    messagesUrl: 'http://localhost:8000/api/messages',
+    messagesUrl: 'http://localhost:8000/api/messages' || 'https://okta-profile-app.herokuapp.com/api/messages',
   },
   customButtons: [{
     title: 'Click Me 1',
